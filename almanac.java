45e0c7a2f182ac335b6afa736070e01b51e5199c
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2022-10-17 09:50:26 +0000

    Add files via upload
    now.

---
+++ almanac.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author terra6partner
+ */
+public class almanac {
+    
+String[]   SEASONS = {
+    "Spring",
+    "Summer",
+    "Autumn",
+    "Winter",
+};
+
+String[] SEASON_EVENTS = {
+    "Vernal Equinox",
+    "Summer Solstice",
+    "Autumnal Equinox",
+    "Winter Solstice",
+};
+
+String[] SEASON_EVENTS_NEUTRAL = {
+    "March Equinox",
+    "June Solstice",
+    "September Equinox",
+    "December Solstice",
+};
+    
+}
