f8685fa96a47f23842acc3862c54c8e319fd0f4e
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2022-10-18 18:00:18 +0000

    Add files via upload
    Updates.

--- almanac.java
+++ almanac.java
@@ -1,3 +1,8 @@
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -29,5 +34,36 @@ String[] SEASON_EVENTS_NEUTRAL = {
     "September Equinox",
     "December Solstice",
 };
+
+String[] MOON_PHASES = {
+        "New Moon",
+        "First Quarter",
+        "Second Quarter",
+        "Third Quarter",
+};
     
-}
+String[] MOON_NODES = {
+     "DESCENDING",
+    "ASCENDING",
+};
+
+String[] CONJUNCTIONS = {
+    "conjunction",
+    "opposition",
+};
+
+String[] TWILIGHTS = {
+    "Night",
+    "Astronomical twilight",
+    "Nautical twilight",
+    "Civil twilight",
+    "Day",
+};
+
+private void season(String Ephemeris) {
+
+    };
+
+private void season(String Ephemeris) {
+
+    };
