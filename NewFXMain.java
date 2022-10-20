10563999c79910204cbb6ef53d554b394a990ed0
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2022-10-20 00:10:40 +0000

    Add files via upload
    off

--- NewFXMain.java
+++ NewFXMain.java
@@ -11,21 +11,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-// https://github.com/skyfielders/python-skyfield/tree/master/examples
-// https://rhodesmill.org/skyfield/
-// https://www.afahc.ro/ro/afases/2016/MATH&IT/CROITORU_OANCEA.pdf
-// http://www.satobs.org/element.html
-// https://rhodesmill.org/skyfield/examples.html#what-time-is-solar-noon-when-the-sun-transits-the-meridian
-// https://snyk.io/advisor/python/skyfield/functions/skyfield.api
-// https://rhodesmill.org/skyfield/earth-satellites.html
-// https://github.com/skyfielders/python-skyfield/blob/master/skyfield/almanac.py
-// https://www.javatpoint.com/how-to-get-utc-time-in-java
-// https://www.orekit.org/static/apidocs/org/orekit/propagation/analytical/tle/TLE.html
-// https://www.w3schools.com/java/java_arrays.asp
-// https://www.mdpi.com/1424-8220/22/8/2902/htm
-// google: satellite tracking using two line element
-
-
 /**
  *
  * @author terra6partner
