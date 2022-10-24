998d33c79f9a43b104af91981ca879dd8a12d435
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2022-10-24 09:00:10 +0000

    Add files via upload
    Updates.

--- rosetta.java
+++ rosetta.java
@@ -9,6 +9,13 @@
  */
 public class rosetta {
     
+    private static String current_season;
+    private static String current_conjunctions;
+    private static String current_season_events;
+    private static String current_season_lingo;
+    private static String current_daily_twilights;
+    private static String current_nodes;
+    
     String 
      SEASON[], 
      SEASON_EVENTS[],
@@ -23,10 +30,70 @@ public class rosetta {
         this.CONJUNCTIONS = new String[]{"Opposition", "Conjunction"};
         this.DAILY_TWILIGHTS = new String[]{"DAY", "CIVIL TWILIGHT", "NAUTICAL TWILIGHT", "ASTRONOMICAL TWILIGHT", "NIGHT"};
         this.SEASON_EVENTS = new String[]{"Winter Solstice","Autumn Equinox","Summer Solstice","Vernal Equinox"};
-        this.SEASON_LINGO = new String[]{"December Solstice","September Equinox","June Solstice","March Equinox"};
+        this.SEASON_LINGO = new String[]{"Dec Solstice","Sept Equinox","June Solstice","Mar Equinox"};
         this.SEASON = new String[]{"Winter", "Fall", "Summer", "Spring"};
     }   
 
+   private void set_nodes(int nodes)
+    {
+        rosetta.current_nodes = this.NODES[nodes];
+    };
+   
+   private void set_conjunctions(int conjunctions)
+    {
+        rosetta.current_conjunctions = this.CONJUNCTIONS[conjunctions];
+    };
+   
+   private void set_daily_twilights(int daily_twilights)
+    {
+        rosetta.current_daily_twilights = this.DAILY_TWILIGHTS[daily_twilights];
+    };
+   
+   private void set_season_events(int season_events)
+    {
+        rosetta.current_season_events = this.SEASON_EVENTS[season_events];
+    };
+   
+   private void set_current_conjunctions(int current_conjunctions)
+    {
+        rosetta.current_conjunctions = this.CONJUNCTIONS[current_conjunctions];
+    };
+   
+   private void set_current_season(int current_season)
+    {
+        rosetta.current_season = this.SEASON[current_season];
+    };
+   
+   
+   private String get_nodes(int nodes)
+    {
+        return "";
+    };
+   
+   private String get_conjunctions(int conjunctions)
+    {
+        return "";
+    };
+   
+   private String get_daily_twilights(int daily_twilights)
+    {
+        return "";
+    };
+   
+   private String get_season_events(int season_events)
+    {
+        return "";
+    };
+   
+   private String get_current_conjunctions(int current_conjunctions)
+    {
+        return "";
+    };
+   
+   private String get_current_season(int current_season)
+    {
+        return "";
+    };
 }
     
      
