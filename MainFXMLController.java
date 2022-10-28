e1edf0940cb6a41bbc615b087090c5de513191f5
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2022-10-28 10:00:16 +0000

    Add files via upload
    Down

--- MainFXMLController.java
+++ MainFXMLController.java
@@ -3,17 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.paint.Color;
+import javafx.fxml.Initializable;
+import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javafx.fxml.FXML;
+import java.net.URL;
+
 
 /**
  * FXML Controller class
@@ -21,6 +23,10 @@ import javafx.scene.paint.Color;
  * @author terra6partner
  */
 public class MainFXMLController implements Initializable {
+
+    int i = 1;
+    int y = 0;
+    int x = 0;
 
     @FXML
     private TextArea lf1;
@@ -61,7 +67,6 @@ public class MainFXMLController implements Initializable {
     
     @FXML
     private void updateLoadStatus() {
-        int count = 0;
         //String x = line1_fields.getText();
         //Pattern p=Pattern.compile("\\s");  
         //String[] str=p.split(x);  
@@ -70,62 +75,56 @@ public class MainFXMLController implements Initializable {
         //{  
        //    count++; 
        // }     
-       Pattern replace = Pattern.compile("[2]");
-       Matcher matcher2 = replace.matcher(lf1.getText());
-       System.out.println(lf1.getText());
-       System.out.println(matcher2.replaceAll("\t"));
+       //Pattern replace = Pattern.compile("[2]");
+       //Matcher matcher2 = replace.matcher(lf1.getText());
+       //System.out.println(lf1.getText());
+       //System.out.println(matcher2.replaceAll("\t"));
        
-       String text    =
-        "This is the text to be searched " +
-        "for occurrences of the http:// pattern.";
+       //String text    =
+       // "This is the text to be searched " +
+       // "for occurrences of the http:// pattern.";
      
-String tleLineOne = lf1.getText();
-String tleLineTwo = lf2.getText();
+       String tleLineOne = lf1.getText();
+       String tleLineTwo = lf2.getText();
 
-//trim both lines of text
-tleLineOne = tleLineOne.substring(3,tleLineOne.length());
-tleLineTwo = tleLineTwo.substring(3,tleLineTwo.length());
+        //trim both lines of text
+       tleLineOne = tleLineOne.substring(3,tleLineOne.length());
+       tleLineTwo = tleLineTwo.substring(3,tleLineTwo.length());
 
-// extract from tle line 1
-Pattern pattern = Pattern.compile("([1234567890]+)([.1234567890]+)");
-Matcher matcher = pattern.matcher(tleLineOne);
+        // extract from tle line 1
+       Pattern pattern = Pattern.compile("((//+//s)[1234567890]+)([.1234567890]+)");
+       Matcher matcher = pattern.matcher(tleLineOne);
 
-int i = 1;
-int y = 0;
-int x = 0;
-while(matcher.find())
-{
-    
-    x = y;
-    y = matcher.end();
-    System.out.println("x"+x);
-    System.out.println("y"+y);
-    System.out.println(">"+tleLineOne.substring(x+1,y));
-    line_one_array[i]= Double.valueOf(tleLineOne.substring(x+1,y));
-    i++;
-}
+        while(matcher.find())
+        {
 
-// extract from tle line 2
-Pattern pattern_next = Pattern.compile("([1234567890]+)([.1234567890]+)");
-Matcher matcher_next = pattern.matcher(tleLineTwo);
+            x = y;
+            y = matcher.end();
+            //System.out.println("x"+x);
+            //System.out.println("y"+y);
+            //System.out.println(">"+tleLineOne.substring(x+1,y));
+            line_one_array[i]= Double.parseDouble(tleLineOne.substring(x+1,y));
+            i++;
+        }
 
-i = 1;
-y = 0;
-x = 0;
-while(matcher_next.find())
-{
-    x = y;
-    y = matcher_next.end();
-    System.out.println("x"+x);
-    System.out.println("y"+y);
-    System.out.println("><><"+tleLineTwo.substring(x+1,y));
-    line_two_array[i]= Double.valueOf(tleLineTwo.substring(x+1,y));
-    i++;
-}
+        // extract from tle line 2
+        Matcher matcher_next = pattern.matcher(tleLineTwo);
 
-
-label_slot_status.setText("OK - Set");
-label_slot_status.setTextFill(Color.web("#228B22"));
+        i = 1;
+        y = 0;
+        x = 0;
+        while(matcher_next.find())
+        {
+            x = y;
+            y = matcher_next.end();
+            //System.out.println("x"+x);
+            //System.out.println("y"+y);
+            //System.out.println("><><"+tleLineTwo.substring(x+1,y));
+            line_two_array[i]= Double.parseDouble(tleLineTwo.substring(x+1,y));
+            i++;
+        }
+        label_slot_status.setText("OK - Set");
+        label_slot_status.setTextFill(Color.web("#228B22"));
 
     }
     
