4bf8e63655123088089e5279df4a4e5750f87d99
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2022-11-06 23:21:54 +0000

    Add files via upload
    down

--- MainFXMLController.java
+++ MainFXMLController.java
@@ -324,8 +324,16 @@ public class MainFXMLController implements Initializable {
            if (x.equals("-"))
                    {
                     total++;
-                    System.out.println("out>>"+i);
+                    
                    }
+            else if (x.equals("0") | x.equals("1") | x.equals("2") | x.equals("3")| x.equals("4")| x.equals("5")| x.equals("6")| x.equals("7")| x.equals("8")| x.equals("9"))
+               {
+                   System.out.println("total>>"+total);
+                    System.out.println("letter>>"+Integer.valueOf(x));
+                    total = total + Integer.valueOf(x);
+                    
+                    System.out.println("out>>"+i);
+               }
             i++;
         
         }
