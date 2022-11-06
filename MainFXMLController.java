5cd23c52d644700d0f41e4407e97029e4cf6c1e6
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2022-11-06 23:31:14 +0000

    Add files via upload
    down

--- MainFXMLController.java
+++ MainFXMLController.java
@@ -323,8 +323,7 @@ public class MainFXMLController implements Initializable {
            String x = (String) strArray.subSequence(i,i+1);
            if (x.equals("-"))
                    {
-                    total++;
-                    
+                        total++;
                    }
             else if (x.equals("0") | x.equals("1") | x.equals("2") | x.equals("3")| x.equals("4")| x.equals("5")| x.equals("6")| x.equals("7")| x.equals("8")| x.equals("9"))
                {
