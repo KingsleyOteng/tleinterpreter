862269ff926f03da62b12f647379041ceb5a5c54
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2022-11-23 14:20:22 +0000

    Add files via upload
    down.

--- MainFXMLController.java
+++ MainFXMLController.java
@@ -177,8 +177,11 @@ public class MainFXMLController implements Initializable {
        // final String line2 = "2 25544 051.6421 063.2734 0007415 308.6263 249.9177 15.74668600414901";
         // data file loaded
        // TLE aOrkitTle = new TLE(line1, line2);
+        
         btn_load_element.setTextFill(Color.RED);
+        
         //mount_label_1.setText("hello");
+        
         populateMounts();
         choiceBox.setItems(mountConfigurationList);
     }
