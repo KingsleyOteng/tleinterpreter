acdbf1fb01d2a79665ebc235cbd6c66400faddb9
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2022-12-02 09:50:17 +0000

    Add files via upload
    down .30

--- MainFXMLController.java
+++ MainFXMLController.java
@@ -168,6 +168,24 @@ public class MainFXMLController implements Initializable {
     private ChoiceBox<String> choiceBox 
             = new ChoiceBox();
     @FXML
+    private ChoiceBox<String> obsMonBox 
+            = new ChoiceBox();
+    @FXML
+    private ChoiceBox<String> obsDayBox 
+            = new ChoiceBox();
+    @FXML
+    private ChoiceBox<String> obsTimeHourBox 
+            = new ChoiceBox();
+    @FXML
+    private ChoiceBox<String> obsYearBox 
+            = new ChoiceBox();
+    @FXML
+    private ChoiceBox<String> obsTimeMnBox 
+            = new ChoiceBox();
+    @FXML
+    private ChoiceBox<String> obsTimeSecBox 
+            = new ChoiceBox();
+    @FXML
     private Label mount_label_1;
     @FXML
     private Label mount_label_2;
@@ -175,9 +193,12 @@ public class MainFXMLController implements Initializable {
     private final MenuItem item1 = new MenuItem();
 
     private ObservableList<String> mountConfigurationList = FXCollections.observableArrayList();
-
-   
-
+    private ObservableList<String> obsDateYearList = FXCollections.observableArrayList();
+    private ObservableList<String> obsDateMonList = FXCollections.observableArrayList();
+    private ObservableList<String> obsDateDayList = FXCollections.observableArrayList();
+    private ObservableList<String> obsTimeHourList = FXCollections.observableArrayList();
+    private ObservableList<String> obsTimeMinList = FXCollections.observableArrayList();
+    private ObservableList<String> obsTimeSecList = FXCollections.observableArrayList();
     /**
      * Update JavaFX file
      *
@@ -208,6 +229,12 @@ public class MainFXMLController implements Initializable {
         btn_load_element.setTextFill(Color.RED);
         this.populateMounts();
         choiceBox.setItems(mountConfigurationList);
+        obsMonBox.setItems(obsDateMonList);
+        obsDayBox.setItems(obsDateDayList);
+        obsTimeHourBox.setItems(obsTimeHourList);
+        obsYearBox.setItems(obsDateYearList);
+        obsTimeMnBox.setItems(obsTimeMinList);
+        obsTimeSecBox.setItems(obsTimeSecList);
         
         // 
         FactoryManagedFrame ITRF = FramesFactory.getITRF(IERSConventions.IERS_2010, true);
@@ -581,6 +608,174 @@ public class MainFXMLController implements Initializable {
         mountConfigurationList.add("Equatorial");
         mountConfigurationList.add("Dobsonian");
         mountConfigurationList.add("German equatorial");
+        
+         // hours list for choicebox
+        obsTimeHourList.add("00");
+        obsTimeHourList.add("01");
+        obsTimeHourList.add("02");
+        obsTimeHourList.add("03");
+        obsTimeHourList.add("04");
+        obsTimeHourList.add("05");
+        obsTimeHourList.add("06");
+        obsTimeHourList.add("07");
+        obsTimeHourList.add("08");
+        obsTimeHourList.add("09");
+        obsTimeHourList.add("10");
+        obsTimeHourList.add("11");
+        obsTimeHourList.add("12");
+        obsTimeHourList.add("13");
+        obsTimeHourList.add("14");
+        obsTimeHourList.add("15");
+        obsTimeHourList.add("16");
+        obsTimeHourList.add("17");
+        obsTimeHourList.add("18");
+        obsTimeHourList.add("19");
+        obsTimeHourList.add("20");
+        obsTimeHourList.add("21");
+        obsTimeHourList.add("22");
+        obsTimeHourList.add("23");
+   
+        // minutes list for choicebox
+        obsTimeMinList.add("00");
+        obsTimeMinList.add("01");
+        obsTimeMinList.add("02");
+        obsTimeMinList.add("03");
+        obsTimeMinList.add("04");
+        obsTimeMinList.add("05");
+        obsTimeMinList.add("06");
+        obsTimeMinList.add("07");
+        obsTimeMinList.add("08");
+        obsTimeMinList.add("09");
+        obsTimeMinList.add("10");
+        obsTimeMinList.add("11");
+        obsTimeMinList.add("12");
+        obsTimeMinList.add("13");
+        obsTimeMinList.add("14");
+        obsTimeMinList.add("15");
+        obsTimeMinList.add("16");
+        obsTimeMinList.add("17");
+        obsTimeMinList.add("18");
+        obsTimeMinList.add("19");
+        obsTimeMinList.add("20");
+        obsTimeMinList.add("21");
+        obsTimeMinList.add("22");
+        obsTimeMinList.add("23");
+        obsTimeMinList.add("24");
+        obsTimeMinList.add("25");
+        obsTimeMinList.add("26");
+        obsTimeMinList.add("27");
+        obsTimeMinList.add("28");
+        obsTimeMinList.add("29");
+        obsTimeMinList.add("30");
+        obsTimeMinList.add("31");
+        obsTimeMinList.add("32");
+        obsTimeMinList.add("33");
+        obsTimeMinList.add("34");
+        obsTimeMinList.add("35");
+        obsTimeMinList.add("36");
+        obsTimeMinList.add("37");
+        obsTimeMinList.add("38");
+        obsTimeMinList.add("39");
+        obsTimeMinList.add("40");
+        obsTimeMinList.add("41");
+        obsTimeMinList.add("42");
+        obsTimeMinList.add("43");
+        obsTimeMinList.add("44");
+        obsTimeMinList.add("45");
+        obsTimeMinList.add("46");
+        obsTimeMinList.add("47");
+        obsTimeMinList.add("48");
+        obsTimeMinList.add("49");
+        obsTimeMinList.add("50");
+        obsTimeMinList.add("51");
+        obsTimeMinList.add("52");
+        obsTimeMinList.add("53");
+        obsTimeMinList.add("54");
+        obsTimeMinList.add("55");
+        obsTimeMinList.add("56");
+        obsTimeMinList.add("57");
+        obsTimeMinList.add("58");
+        obsTimeMinList.add("59");
+        
+        // seconds list for choicebox
+        obsTimeSecList.add("00");
+        obsTimeSecList.add("15");
+        obsTimeSecList.add("30");
+        obsTimeSecList.add("45");
+        
+        // years list for choicebox
+        obsDateYearList.add("1999");
+        obsDateYearList.add("2001");
+        obsDateYearList.add("2002");
+        obsDateYearList.add("2003");
+        obsDateYearList.add("2004");
+        obsDateYearList.add("2005");
+        obsDateYearList.add("2006");
+        obsDateYearList.add("2007");
+        obsDateYearList.add("2008");
+        obsDateYearList.add("2009");
+        obsDateYearList.add("2010");
+        obsDateYearList.add("2011");
+        obsDateYearList.add("2012");
+        obsDateYearList.add("2013");
+        obsDateYearList.add("2014");
+        obsDateYearList.add("2015");
+        obsDateYearList.add("2016");
+        obsDateYearList.add("2017");
+        obsDateYearList.add("2018");
+        obsDateYearList.add("2019");
+        obsDateYearList.add("2020");
+        obsDateYearList.add("2021");
+        obsDateYearList.add("2022");
+        obsDateYearList.add("2023");
+        
+        // day month list for choicebox
+        obsDateMonList.add("JAN");
+        obsDateMonList.add("FEB");
+        obsDateMonList.add("MAR");
+        obsDateMonList.add("APR");
+        obsDateMonList.add("MAY");
+        obsDateMonList.add("JUN");
+        obsDateMonList.add("JUL");
+        obsDateMonList.add("AUG");
+        obsDateMonList.add("SEPT");
+        obsDateMonList.add("OCT");
+        obsDateMonList.add("NOVE");
+        obsDateMonList.add("DEC");
+       
+        // day date list for choicebox
+        obsDateDayList.add("01");
+        obsDateDayList.add("02");
+        obsDateDayList.add("03");
+        obsDateDayList.add("04");
+        obsDateDayList.add("05");
+        obsDateDayList.add("06");
+        obsDateDayList.add("07");
+        obsDateDayList.add("08");
+        obsDateDayList.add("09");
+        obsDateDayList.add("10");
+        obsDateDayList.add("11");
+        obsDateDayList.add("12");
+        obsDateDayList.add("13");
+        obsDateDayList.add("14");
+        obsDateDayList.add("15");
+        obsDateDayList.add("16");
+        obsDateDayList.add("17");
+        obsDateDayList.add("18");
+        obsDateDayList.add("19");
+        obsDateDayList.add("20");
+        obsDateDayList.add("21");
+        obsDateDayList.add("22");
+        obsDateDayList.add("23");
+        obsDateDayList.add("24");
+        obsDateDayList.add("25");
+        obsDateDayList.add("26");
+        obsDateDayList.add("27");
+        obsDateDayList.add("28");
+        obsDateDayList.add("29");
+        obsDateDayList.add("30");
+        obsDateDayList.add("31");
+
     }
     
 }
