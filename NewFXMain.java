2733d400d742f8f212be6e187742067e2abc6e06
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2022-12-02 10:00:57 +0000

    Add files via upload
    down .30

--- NewFXMain.java
+++ NewFXMain.java
@@ -23,20 +23,20 @@ public class NewFXMain extends Application {
      */ 
     @Override
     public void start(Stage stage) throws Exception, InvocationTargetException
-    {
-        Parent root = FXMLLoader.load(getClass().getResource("MainFXML.fxml"));
-        Scene scene = new Scene(root, 860, 880);
-        stage.setTitle("TLE Interpreter");
-        stage.setScene(scene);
-        stage.show();
-    }
+        {
+            Parent root = FXMLLoader.load(getClass().getResource("MainFXML.fxml"));
+            Scene scene = new Scene(root, 860, 880);
+            stage.setTitle("TLE Interpreter");
+            stage.setScene(scene);
+            stage.show();
+        }
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) 
-    {
-        launch(args);
-    }
+        {
+            launch(args);
+        }
     
 }
