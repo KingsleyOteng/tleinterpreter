9ef137890739aedd58e3d3693b8e286f113f6132
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2022-12-06 16:12:13 +0000

    Add files via upload
    down .30

--- MainFXMLController.java
+++ MainFXMLController.java
@@ -897,7 +897,6 @@ public class MainFXMLController implements Initializable {
                     .setValue(String.format("%d",localTime.getMinute()));
             obsTimeSecBox
                     .setValue("00");
-            
         }
     
 }
