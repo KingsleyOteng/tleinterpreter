31a6037c1b5b4735981a6bed0af78c192b8b1f8a
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2023-01-25 14:00:33 +0000

    Add files via upload
    down .30

--- NewFXMain.java
+++ NewFXMain.java
@@ -37,10 +37,12 @@ public class NewFXMain extends Application
                 stage.setScene(scene);
                 stage.show();
             }
+      
         
         /**
          * @param args the command line arguments
          */
+        
         
         public static void main(String[] args) 
             {
