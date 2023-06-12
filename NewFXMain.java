b02b8fa03844f12417af5e05c19d33b50b7e47c7
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2023-06-12 20:01:03 +0000

    Add files via upload
    down .20

--- NewFXMain.java
+++ NewFXMain.java
@@ -3,27 +3,38 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
  */
 
+/*
+@Dates: May 26th 2022
+@author kingsley oteng-amoako
+*/
+
+
 import java.lang.reflect.InvocationTargetException;
 import javafx.application.Application;
-//import javafx.event.ActionEvent;
-//import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
 
     /**
-     * @author terra6partners
+     * @author terra6 partners
      */
+
+
+
 
 public class NewFXMain extends Application 
     {
+    
         /**
          * construct the UI
          * @param stage
          * @throws java.lang.Exception
          * @throws java.lang.reflect.InvocationTargetException
          */ 
+    
         @Override
         public void start(Stage stage) throws Exception, InvocationTargetException
             {
@@ -33,7 +44,8 @@ public class NewFXMain extends Application
                 stage.setScene(scene);
                 stage.show();
             }
-
+      
+        
         /**
          * @param args the command line arguments
          */
