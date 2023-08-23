37047910398a319012b0e785e7229323d99fd1f8
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2023-08-23 15:50:30 +0000

    Add files via upload
    down .20

--- Tools.java
+++ Tools.java
@@ -1,5 +1,8 @@
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -12,15 +15,36 @@ import java.io.File;
  */
 public class Tools 
 {
+   public static final String delimiter = ",";
+   public static void read(String csvFile) throws java.io.IOException {
+      try {
+                File file = new File(csvFile);
+                FileReader fr = new FileReader(file);
+                BufferedReader br = new BufferedReader(fr);
+                String line = "";
+                String[] tempArr;
+                while((line = br.readLine()) != null) 
+                   {
+                       tempArr = line.split(delimiter);
+                       for(String tempStr : tempArr) {
+                          System.out.print(tempStr + " ");
+                       }
+                       System.out.println();
+                   }
+                br.close();
+         } 
+            catch(IOException ioe) 
+         {
+            ioe.printStackTrace();
+         }
+   }
+   
+      public static void main(String[] args) throws IOException {
+      // csv file to read
+      String csvFile = "norad_id_match.csv";
+      Tools.read(csvFile);
+   }
     
-    private String get_object_name(int id)
-    {
-        File file = new File(
-            "");
-        
-        String value_string = "";
-        
-        return value_string;
-    };
+
     
 }
